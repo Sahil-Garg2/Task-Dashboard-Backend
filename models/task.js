73847b6632b81ba4c1032276cacdf3d3dd877c9b
Sahil Garg <43277712+Sahil-Garg2@users.noreply.github.com>
2025-01-13 21:38:50 +0530

    Update task.js

--- models/task.js
+++ models/task.js
@@ -3,13 +3,21 @@ const taskSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: String,
   assignedUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  assignedRole: { type: mongoose.Schema.Types.ObjectId, ref:'Role', required: true },
   category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' },
-  checklist: [String],
-  dependencies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task' }],
+  checklist: [{
+    id: { type: mongoose.Schema.Types.ObjectId, ref: 'Checklist' },
+    status: {
+      type: String,
+      enum: ['pending', 'in-progress', 'completed'],
+      default: 'pending',
+    }
+  }],
+  dependency: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Role' }],
+  currentDependencyNo: { type: Number, default: 0 },
   status: {
     type: String,
-    enum: ['pending', 'in-progress', 'completed'],
-    default: 'pending',
+    ref:'Role'
   },
 });
 module.exports = mongoose.model('Task', taskSchema);
