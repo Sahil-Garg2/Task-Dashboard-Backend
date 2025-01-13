72e6ca0fa4e79498f43c818edfdf3479705a5eb3
Sahil Garg <43277712+Sahil-Garg2@users.noreply.github.com>
2025-01-13 21:42:10 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,48 @@
 const express = require('express');
 const User = require('../models/user');
+const Role = require('../models/role');
+const Category = require('../models/category');
 const { protect, isAdmin } = require('../middleware/authMiddleware');
 const router = express.Router();
 
+
+router.get("/", protect, isAdmin, async (req, res) => {
+  try {
+    const users = await User.find().populate('role').populate('categories.category');
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching users', error: error.message });
+  }
+});
+
 // Create new user (Admin only)
 router.post('/create', protect, isAdmin, async (req, res) => {
-  const { username, email, password, role } = req.body;
+  const { username, email, password, role, categories } = req.body;
 
   try {
-    const user = new User({ username, email, password, role });
+    // Check if the role exists
+    const assignedRole = await Role.findById(role);
+    if (assignedRole?.length === 0) {
+      return res.status(400).json({ message: 'Invalid role' });
+    }
+
+    // Check if categories exist
+    const categoryIds = categories.map((cat) => cat.category);
+    const categoriesFromDb = await Category.find({ _id: { $in: categoryIds } });
+    if (categoriesFromDb.length !== categories.length) {
+      return res.status(400).json({ message: 'Invalid categories provided' });
+    }
+
+
+    const user = new User({
+      username, email, password, role, categories: categories.map((cat) => ({
+        category: cat.category,
+        assignedRole: cat.assignedRole,
+      })) });
     await user.save();
     res.status(201).json(user);
   } catch (error) {
+    console.log(error);
     res
       .status(500)
       .json({ message: 'Error creating user', error: error.message });
