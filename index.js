aa04813b0d156ef6d0fe6f5b85843a5df24e6910
Sahil Garg <43277712+Sahil-Garg2@users.noreply.github.com>
2025-01-13 21:42:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const { resolve } = require('path');
 const dotenv = require('dotenv');
+const Role = require('./models/role');
 // Import routes
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const categoryRoutes = require('./routes/categoryRoutes');
 const taskRoutes = require('./routes/taskRoutes');
+const roleRoutes = require('./routes/roleRoutes');
+const checklist = require('./routes/checkListRoute');
 
 const app = express();
 const port = 3010;
@@ -20,22 +23,19 @@ app.use(bodyParser.json());
 app.use(express.static('static'));
 
 mongoose
-  .connect('mongodb://localhost:27017/task-management-system', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  .connect('mongodb+srv://sahilgarg99912:kZ8dXtaHvezRmrAU@cluster0.jq62e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
   })
   .then(() => console.log('MongoDB Connected'))
   .catch((err) => console.log('MongoDB Connection Error: ', err));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
 
 // Use routes
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/tasks', taskRoutes);
+app.use('/api/roles', roleRoutes);
+app.use('/api/checklist', checklist);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
